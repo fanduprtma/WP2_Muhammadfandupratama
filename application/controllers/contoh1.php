7c68a6493fcc21a2c65362a7374a42d54823b180
fanduprtma <muhammadfandu25@gmail.com>
2023-04-06 08:56:49 +0700

    P3

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Perkenalkan nama Muhammad Fandu Pratama. 
+        Nim saya adalah 17200263.
+        Saya berada di kelas 17.6A.01 .
+        Saya tinggal di Kota Depok.
+        Hobi saya adalah
+        Biliar";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Muhammad  fandu Pratama</li>
+ <li>Nama Belakang : Fandu</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Nangka No 32</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Depok/li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Basket</li>
+ <li>Renang</li>
+ </ul>
+</section>
